87c47dca867e9f00b1af1674cd8231654c0c5684
OBEPYC <obepyc@mail.ru>
2016-01-11 00:40:34 +0200

    comments + gallery

--- index.php
+++ index.php
@@ -1,200 +1,226 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <title>VIP-Korteg 71</title>
-    <link rel="stylesheet" href="css/main.css">
+	<meta charset="UTF-8">
+	<title>VIP-Korteg 71</title>
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <header>
-        <div class="wrap">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 col-sm-3 col-xs-3">
-                        <div class="logo">
-                            <a href="/" title="VIP-Korteg 71">
-                                <img src="img/logo.png" alt="VIP-Korteg 71">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-9 col-sm-9 col-xs-9">
-                        <div class="menu">
-                            <ul>
-                                <li><a href="">Главная</a></li>
-                                <li><a href="">Преимущества</a></li>
-                                <li><a href="">Услуги</a></li>
-                                <li><a href="">Автопарк</a></li>
-                                <li><a href="">Отзывы</a></li>
-                                <li><a href="">Контакты</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="clr"></div>
-                </div>
-                <div class="row">
-                    <div class="headet_text">
-                        <h1>Cадебный кортеж в Туле! Сделайте Вашу <br>
-                           свадьбу незабываемой!
-                       </h1>
-                   </div>
-               </div>
-           </div>
-       </div>
-   </header>
+	<header>
+		<div class="wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="logo">
+							<a href="/" title="VIP-Korteg 71">
+								<img src="img/logo.png" alt="VIP-Korteg 71">
+							</a>
+						</div>
+					</div>
+					<div class="col-md-9 col-sm-9 col-xs-9">
+						<div class="menu">
+							<ul>
+								<li><a href="">Главная</a></li>
+								<li><a href="">Преимущества</a></li>
+								<li><a href="">Услуги</a></li>
+								<li><a href="">Автопарк</a></li>
+								<li><a href="">Отзывы</a></li>
+								<li><a href="">Контакты</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="clr"></div>
+				</div>
+				<div class="row">
+					<div class="headet_text">
+						<h1>Cадебный кортеж в Туле! Сделайте Вашу <br>
+							свадьбу незабываемой!
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
 
-   <!--    Преимущества-->
+	<!--    Преимущества-->
 
-   <section class="adva">
-    <div class="warp">
-        <div class="adva_left"></div>
-        <div class="adva_right"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-3 col-xs-3"></div>
-                <div class="col-md-6 col-sm-6 col-xs-6">
-                    <div class="adv_cont">
-                        <div class="adva_inf">- Почему нас выбирают? -</div>
-                        <ul class="adva_item">
-                            <li>
-                                <span>1</span> Мы знаем свое дело и выполним свою работу на самом высоком уровне.
-                            </li>
-                            <li>
-                                <span>2</span> Весь комплекс услуг для проведения вашего торжества.
-                            </li>
-                            <li>
-                                <span>3</span> Огромный выбор автомобилей на любой вкус и кошелек, от 
-                                эконом класса до ретро автомобилей.
-                            </li>
-                            <li>
-                                <span>4</span> Наши цены полностью прозрачны и фиксированы, 
-                                никаких лишних расчетов.
-                            </li>
-                            <li>
-                                <span>5</span> Опытные менеджеры организовавшие и выполнившие ни один десяток торжеств.
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3 col-xs-3"></div>
-                <div class="clr"></div>
-            </div>
-        </div>
-    </div>
-</section>
+	<section class="adva">
+		<div class="warp">
+			<div class="adva_left"></div>
+			<div class="adva_right"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-sm-3 col-xs-3"></div>
+					<div class="col-md-6 col-sm-6 col-xs-6">
+						<div class="adv_cont">
+							<div class="adva_inf">- Почему нас выбирают? -</div>
+							<ul class="adva_item">
+								<li>
+									<span>1</span> Мы знаем свое дело и выполним свою работу на самом высоком уровне.
+								</li>
+								<li>
+									<span>2</span> Весь комплекс услуг для проведения вашего торжества.
+								</li>
+								<li>
+									<span>3</span> Огромный выбор автомобилей на любой вкус и кошелек, от 
+									эконом класса до ретро автомобилей.
+								</li>
+								<li>
+									<span>4</span> Наши цены полностью прозрачны и фиксированы, 
+									никаких лишних расчетов.
+								</li>
+								<li>
+									<span>5</span> Опытные менеджеры организовавшие и выполнившие ни один десяток торжеств.
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-3"></div>
+					<div class="clr"></div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-<!--    Услуги-->
+	<!--    Услуги-->
 
-<section class="serv">
-    <div class="serv_top"></div>
-    <div class="serv_bot"></div>
-    <div class="wrap">
-        <div class="container">
-            <div class="row">
-                <div class="serv_inf">- Наши услуги -</div>
-            </div>
-            <div class="row">
-             <div class="col-md-2 col-sm-2 col-xs-2"></div>
-             <div class="col-md-4 col-sm-4 col-xs-4">
-                <div class="serv_item">
-                    <p>Организация свадебного кортежа</p>
-                    <img src="img/serv_item_1.png" alt="">
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4 col-xs-4">                    
-                <div class="serv_item">
-                    <p>Подбор и оформление украшений</p>
-                    <img src="img/serv_item_2.png" alt="">
-                </div>
-            </div>
-            <div class="col-md-2 col-sm-2 col-xs-2"></div>
-            <div class="clr"></div>
-        </div>
-        <div class="row">
-         <div class="col-md-2 col-sm-2 col-xs-2"></div>
-         <div class="col-md-4 col-sm-4 col-xs-4">
-            <div class="serv_item">
-                <p>Трансферы по России</p>
-                <img src="img/serv_item_3.png" alt="">
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-4 col-xs-4">                    
-            <div class="serv_item">
-                <p>Фотосессия на автомобиле</p>
-                <img src="img/serv_item_4.png" alt="">
-            </div>
-        </div>
-        <div class="col-md-2 col-sm-2 col-xs-2"></div>
-        <div class="clr"></div>
-    </div>
-</div>
-</div>
-</section>
+	<section class="serv">
+		<div class="serv_top"></div>
+		<div class="serv_bot"></div>
+		<div class="wrap">
+			<div class="container">
+				<div class="row">
+					<div class="serv_inf">- Наши услуги -</div>
+				</div>
+				<div class="row">
+					<div class="col-md-2 col-sm-2 col-xs-2"></div>
+					<div class="col-md-4 col-sm-4 col-xs-4">
+						<div class="serv_item">
+							<p>Организация свадебного кортежа</p>
+							<img src="img/serv_item_1.png" alt="">
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-4">                    
+						<div class="serv_item">
+							<p>Подбор и оформление украшений</p>
+							<img src="img/serv_item_2.png" alt="">
+						</div>
+					</div>
+					<div class="col-md-2 col-sm-2 col-xs-2"></div>
+					<div class="clr"></div>
+				</div>
+				<div class="row">
+					<div class="col-md-2 col-sm-2 col-xs-2"></div>
+					<div class="col-md-4 col-sm-4 col-xs-4">
+						<div class="serv_item">
+							<p>Трансферы по России</p>
+							<img src="img/serv_item_3.png" alt="">
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-4">                    
+						<div class="serv_item">
+							<p>Фотосессия на автомобиле</p>
+							<img src="img/serv_item_4.png" alt="">
+						</div>
+					</div>
+					<div class="col-md-2 col-sm-2 col-xs-2"></div>
+					<div class="clr"></div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-<!-- Автопарк -->
+	<!-- Автопарк -->
 
-<section class="gallery">
-    <div class="container">
-        <div class="row">
-           <div class="gallery_inf">- Наш автопарк -</div>
-           <div class="col-md-6 col-sm-6 col-xs-6">
-               <div class="main_gal">
-                   <img id="gal_main_img" src="" alt="">
-                   <p class="avto_price">123</p>
-               </div>
-           </div>
-           <div class="col-md-6 col-sm-6 col-xs-6">
-               <div class="mini_gal" id="sub_gal">
-                    
-               </div>
-           </div>
-       </div>
-   </div>
-</section>
+	<section class="gallery">
+		<div class="container">
+			<div class="row">
+				<div class="gallery_inf">- Наш автопарк -</div>
+				<div class="col-md-6 col-sm-6 col-xs-6">
+					<div class="main_gal">
+						<img id="gal_main_img" src="" alt="">
+						<p class="avto_price"> - Лимузины от - 20 000 руб. -</p>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6 col-xs-6">
+					<div class="mini_gal" id="sub_gal"></div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-<section class="comment">
-    <div class="container">
-        <div class="row"></div>
-    </div>
-</section>
+	<section class="comment">
+		<div class="comment_left"></div>
+		<div class="comment_right"></div>
+		<div class="container">
+			<div class="row">
+				<div class="comment_inf">- Отзывы о нас -</div>
+				<div class="col-md-3 col-sm-3 col-xs-3"></div>
+				<div class="col-md-6 col-sm-6 col-xs-6">
+					<div class="all_comments">
+						<div class="comment_item">
+							<p><b class="name">Натьля Шкаева</b> <i class="date">01.01.2016</i></p>
+							<p class="text">
+								Спасибо Вам за обслуживание нашей свадьбы. Все автомобили приехали вовремя, без опозданий!
+							</p>
+						</div>	
+						<div class="comment_item">
+							<p><b class="name">Натьля Шкаева</b> <i class="date">01.01.2016</i></p>
+							<p class="text">
+								Спасибо Вам за обслуживание нашей свадьбы. Все автомобили приехали вовремя, без опозданий!
+							</p>
+						</div>	
+						<div class="comment_item">
+							<p><b class="name">Натьля Шкаева</b> <i class="date">01.01.2016</i></p>
+							<p class="text">
+								Спасибо Вам за обслуживание нашей свадьбы. Все автомобили приехали вовремя, без опозданий!
+							</p>
+						</div>	
+						<div class="comment_item">
+							<p><b class="name">Натьля Шкаева</b> <i class="date">01.01.2016</i></p>
+							<p class="text">
+								Спасибо Вам за обслуживание нашей свадьбы. Все автомобили приехали вовремя, без опозданий!
+							</p>
+						</div>	
+						<div class="comment_item">
+							<p><b class="name">Натьля Шкаева</b> <i class="date">01.01.2016</i></p>
+							<p class="text">
+								Спасибо Вам за обслуживание нашей свадьбы. Все автомобили приехали вовремя, без опозданий!
+							</p>
+						</div>	
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-3"></div>
+			</div>
+		</div>
+	</section>
 
-<!-- Script -->
-<script src="js/jquery.js"></script>
+	<!-- Script -->
+	<script src="js/jquery.js"></script>
 
-<script type="text/javascript">
-    <?php
-    $images = array();
-    $folder = 'img/cars/';
-    $array_to_js='var imgs=[';
-    $all_files = scandir($folder);
-    for ($i=2; $i<=sizeof($all_files)-1; $i++) {
-        $array_to_js.='"'.$folder.$all_files[$i].'",';
-    }
-    echo substr($array_to_js,0,-1).'];';
-    ?>
+	<script type="text/javascript">
+		<?php
+		$images = array();
+		$folder = 'img/cars/';
+		$array_to_js='var imgs=[';
+		$all_files = scandir($folder);
+		for ($i=2; $i<=sizeof($all_files)-1; $i++) {
+			$array_to_js.='"'.$folder.$all_files[$i].'",';
+		}
+		echo substr($array_to_js,0,-1).'];';
+		?>
 
-    // var n=0;
-    // var time=800;
-    // play=setInterval("chgImg()", 3000);
-
-    var doc = document.getElementById("sub_gal");
-    // function chgImg() {
-    //     $('#gal_main_img1').fadeOut(time, function() {
-    //       $(this).attr('src', imgs[n]).fadeIn(time);
-    //   });
-
-    //     n++;
-    //     if (n>=imgs.length) n=0;
-    // }
-    for(var i = 2; i < imgs.length; i++){
-        doc.innerHTML += '<div class="col-md-4 col-sm-4 col-xs-4"><img src="'+ imgs[i] +'" alt=""></div>';
-    }
-    doc.innerHTML += '<div class="clr"></div>';
-    $("#sub_gal img").on("click", function(){
-        $(".main_gal img").attr('src', $(this).attr('src'));
-        console.log($(this).attr('src'));
-    });
-    console.log(imgs.length);
-</script>
+		var doc = document.getElementById("sub_gal");
+		for(var i = 2; i < imgs.length; i++){
+			doc.innerHTML += '<div class="col-md-4 col-sm-4 col-xs-4"><img src="'+ imgs[i] +'" alt=""></div>';
+		}
+		doc.innerHTML += '<div class="clr"></div>';
+		$(".main_gal img").attr('src', imgs[2]);
+		$("#sub_gal img").on("click", function(){
+			$(".main_gal img").attr('src', $(this).attr('src'));
+			console.log($(this).attr('src'));
+		});
+	</script>
 
 </body>
 </html>
